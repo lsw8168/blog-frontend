63e8ff01caa3438f3a80e92b63e147bfb7794b84
wonni <lsw8168@naver.com>
2020-01-07 08:45:21 +0900

    페이징 기능

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
 import PostListContainer from '../containers/posts/PostListContainer';
+import PaginationContainer from '../containers/posts/PaginationContainer';
 
 function PostListPage() {
   return (
     <>
       <HeaderContainer />
       <PostListContainer />
+      <PaginationContainer />
     </>
   );
 }
